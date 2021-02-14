fa12e990ec7ea452eb465ebe05a5093892f4cd71
Mo <moziauddin@gmail.com>
2021-02-14 23:25:16 +1100

    added job and for loop

--- 1-hello.groovy
+++ 1-hello.groovy
@@ -2,4 +2,9 @@ node {
         stage("Hello world"){
             println 'hello'
         }
+        stage('Groovy loop') {
+            5.times {
+                println "2 * $it = ${2 * it}"
+            }
+        }
 }
